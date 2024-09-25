68a3c0833f21cea782d41d92ee5d783b6609595c
Abdou Aziz Ndiaye <abdouaziznjay@gmail.com>
2024-09-25 02:13:00 +0000

    adding new functionality

--- actions/getCustomers.ts
+++ actions/getCustomers.ts
@@ -1,18 +1,18 @@
-import { prisma } from "@/lib/prisma";
-import { Customer } from "@prisma/client";
+// import { prisma } from "@/lib/prisma";
+// import { Customer } from "@prisma/client";
 
-const getCustomers = async (): Promise<Customer[]> => {
-  try {
-    const customers = await prisma.customer.findMany({
-      where: {
-        type: "SENDER",
-      },
-    });
-    return customers;
-  } catch (error) {
-    console.log("[ET_CUSTOMERS]", error);
-    return [];
-  }
-};
+// const getCustomers = async (): Promise<Customer[]> => {
+//   try {
+//     const customers = await prisma.customer.findMany({
+//       where: {
+//         type: "SENDER",
+//       },
+//     });
+//     return customers;
+//   } catch (error) {
+//     console.log("[ET_CUSTOMERS]", error);
+//     return [];
+//   }
+// };
 
-export default getCustomers;
+// export default getCustomers;
